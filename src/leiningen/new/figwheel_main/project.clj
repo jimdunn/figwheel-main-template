c9d9dff20bb741d6bce23baef916d7d8166c3f6c
jimdunn <james.o.dunn@gmail.com>
2020-05-10 23:21:10 -0700

    Update project.clj

--- src/leiningen/new/figwheel_main/project.clj
+++ src/leiningen/new/figwheel_main/project.clj
@@ -4,21 +4,21 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :min-lein-version "2.7.1"
+  :min-lein-version "2.9.3"
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.520"]{{#react?}}
-                 [cljsjs/react "16.4.1-0"]
-                 [cljsjs/react-dom "16.4.1-0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.758"]{{#react?}}
+                 [cljsjs/react "16.13.1-0"]
+                 [cljsjs/react-dom "16.13.1-0"]
                  [cljsjs/create-react-class "15.6.3-1"]
-                 [sablono "0.8.4"]{{/react?}}{{#om?}}
-                 [cljsjs/react "16.4.1-0"]
-                 [cljsjs/react-dom "16.4.1-0"]
+                 [sablono "0.8.6"]{{/react?}}{{#om?}}
+                 [cljsjs/react "16.13.1-0"]
+                 [cljsjs/react-dom "16.13.1-0"]
                  [cljsjs/create-react-class "15.6.3-1"]
-                 [sablono "0.8.4"]
+                 [sablono "0.8.6"]
                  [org.omcljs/om "1.0.0-beta4"]{{/om?}}{{#reagent?}}
-                 [reagent "0.8.1"]{{/reagent?}}{{#rum?}}
-                 [rum "0.11.2"]{{/rum?}}]
+                 [reagent "1.0.0-alpha1"]{{/reagent?}}{{#rum?}}
+                 [rum "0.11.5"]{{/rum?}}]
 
   :source-paths ["src"]
 
@@ -27,7 +27,7 @@
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" "{{test-runner-ns}}"]}
 
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.3"]{{^windows?}}
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.4"]{{^windows?}}
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]{{/windows?}}]
                    {{#deps?}}:resource-paths ["target"]
                    ;; need to add the compiled assets to the :clean-targets
