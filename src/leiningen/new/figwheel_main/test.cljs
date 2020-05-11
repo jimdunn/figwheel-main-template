870e6e3cd2d54ad19b36868a199a98e5c037b404
jimdunn <james.o.dunn@gmail.com>
2020-05-10 23:22:16 -0700

    Update test.cljs

--- src/leiningen/new/figwheel_main/test.cljs
+++ src/leiningen/new/figwheel_main/test.cljs
@@ -1,10 +1,10 @@
 (ns {{namespace}}-test
     (:require
      [cljs.test :refer-macros [deftest is testing]]
-     [{{namespace}} :refer [multiply]]))
+     #_[{{namespace}} :refer [multiply]]))
 
-(deftest multiply-test
+#_(deftest multiply-test
   (is (= (* 1 2) (multiply 1 2))))
 
-(deftest multiply-test-2
+#_(deftest multiply-test-2
   (is (= (* 75 10) (multiply 10 75))))
